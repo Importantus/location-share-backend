774eb76fc43dc8d0a64f32bc4a4edc12b64d0fab
Importantus <importantus@mailbox.org>
2024-12-08 16:35:02 +0100

    🥅 Add error type

---
+++ customerrors/errors.go
@@ -0,0 +1,32 @@
+package customerrors
+
+import "net/http"
+
+type APIError struct {
+	Status  int    `json:"status"`
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func (e APIError) Error() string {
+	return e.Message
+}
+
+var (
+	Success = APIError{Status: http.StatusOK, Code: 0, Message: "Success"}
+
+	// Common errors - 5000 - 5999
+	ErrInvalidUUID = APIError{Status: http.StatusBadRequest, Code: 5000, Message: "Invalid UUID"}
+
+	// User errors - 1000 - 1999
+	ErrUserExists      = APIError{Status: http.StatusBadRequest, Code: 1001, Message: "Username already exists"}
+	ErrEmailExists     = APIError{Status: http.StatusBadRequest, Code: 1002, Message: "Email already exists"}
+	ErrHashPassword    = APIError{Status: http.StatusInternalServerError, Code: 1003, Message: "Error hashing password"}
+	ErrCreateUser      = APIError{Status: http.StatusInternalServerError, Code: 1004, Message: "Error creating user"}
+	ErrUsernameInvalid = APIError{Status: http.StatusBadRequest, Code: 1005, Message: "Invalid username"}
+	ErrEmailInvalid    = APIError{Status: http.StatusBadRequest, Code: 1006, Message: "Invalid email"}
+	ErrListUsers       = APIError{Status: http.StatusInternalServerError, Code: 1007, Message: "Error listing users"}
+	ErrUserNotFound    = APIError{Status: http.StatusNotFound, Code: 1008, Message: "User not found"}
+	ErrDeleteUser      = APIError{Status: http.StatusInternalServerError, Code: 1009, Message: "Error deleting user"}
+	ErrUpdateUser      = APIError{Status: http.StatusInternalServerError, Code: 1010, Message: "Error updating user"}
+)
